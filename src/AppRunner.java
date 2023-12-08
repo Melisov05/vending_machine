4bcff5b9371d32278b23ff65a424aa12c444b153
Aitemir Melisov <aytemirmelisov@gmail.com>
2023-12-08 15:50:08 +0600

    Coin replenishment realized

--- src/AppRunner.java
+++ src/AppRunner.java
@@ -55,9 +55,15 @@ public class AppRunner {
     }
 
     private void chooseAction(UniversalArray<Product> products) {
+        print(" a - Пополнить баланс");
         showActions(products);
         print(" h - Выйти");
         String action = fromConsole().substring(0, 1);
+        if ("a".equalsIgnoreCase(action)) {
+            coinAcceptor.setAmount(coinAcceptor.getAmount() + 10);
+            print("Вы пополнили баланс на 10");
+            return;
+        }
         try {
             for (int i = 0; i < products.size(); i++) {
                 if (products.get(i).getActionLetter().equals(ActionLetter.valueOf(action.toUpperCase()))) {
